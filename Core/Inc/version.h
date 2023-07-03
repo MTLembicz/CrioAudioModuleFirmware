2b3887ed734188f423a95c9fb38a326d2e5cdeba
Teo314 <mtlembicz@gmail.com>
2023-07-03 17:10:17 +0200

    Version bump

--- Core/Inc/version.h
+++ Core/Inc/version.h
@@ -8,8 +8,6 @@
 #ifndef INC_VERSION_H_
 #define INC_VERSION_H_
 
-#define MAIN_VERSION	"0.1.1"
-
-#define HARDWARE_COMPATIBLE_VERSION		"1.0.0"
+#define MAIN_VERSION	"1.0.0"
 
 #endif /* INC_VERSION_H_ */
